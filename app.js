d5cf03a62887ae372707ce8863c3008e9f96c614
yanjunrush <2489483905@qq.com>
2020-05-22 18:57:05 +0800

    second

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
 //app.js
 App({
   onLaunch: function () {
+    //colorui
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.StatusBar = e.statusBarHeight;
+        let custom = wx.getMenuButtonBoundingClientRect();
+        this.globalData.Custom = custom;  
+        this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
+      }
+    })
+
     // 展示本地存储能力
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
